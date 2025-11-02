d186d3de4954fbe8696a2cf8df94afda4fba5cbf
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-11-01 22:54:44 -0700

    Implemented array in app.js to store form data

--- app.js
+++ app.js
@@ -6,8 +6,37 @@ const PORT = 3002;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
+const datas = [];
+
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`)
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.post('/submit-order', (req, res) => {
+    const data = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        jobtitle: req.body.jobtitle,
+        company: req.body.company,
+        linkurl: req.body.linkurl,
+        email: req.body.email,
+        meet: req.body.meet,
+        other: req.body.other,
+        message: req.body.message,
+        addmail: req.body.addmail,
+        eformat: req.body.eformat
+    };
+
+    datas.push(data);
+    console.log(datas);
+
+    res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+});
+
+app.get('/admin', (req, res) => {
+    res.send(datas);
 });
 
 app.listen(PORT, () => {
